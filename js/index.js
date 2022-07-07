e2367e2c081c4cd6bbe02bfaa458cf862abb33b0
n_53337 <scalewithusef@gmail.com>
2022-07-07 20:46:51 +0100

    some progress i've trying to fix problems...

--- js/index.js
+++ js/index.js
@@ -12,31 +12,81 @@
 //     document.querySelector('.p1-username').innerHTML = rbUsrnm;
 // }
 
-// rondomize cards logic
- 
+// inordred cards 
+
+function play(){
+    document.getElementById('main-container').style.visibility = 'visible';
+    document.getElementById('play').style.visibility = 'hidden';
+
+}
+
 const cardsTptal = 40;
 const cards = [1,2,3,4,5,6,7,10,11,12,1,2,3,4,5,6,7,10,11,12,1,2,3,4,5,6,7,10,11,12,1,2,3,4,5,6,7,10,11,12];
-for(let i = 0; i<cards.length; i++){
-    const rmd = Math.floor(Math.random()*cards.length);
-    const dtd = cards.splice(i, 1)[0];
-    cards.splice(rmd, 0, dtd);
+const rndmOrderCards = ()=>{
+
+    for(let i = 0; i<cards.length; i++){
+        const rmd = Math.floor(Math.random()*cards.length);
+        const dtd = cards.splice(i, 1)[0];
+        cards.splice(rmd, 0, dtd);
+    }
+    console.log(cards);
 }
-console.log(cards);
+window.onload = 
+rndmOrderCards();
 
 // cards for players
 
 const p1_cards = [];
 const p2_cards = [];
+const qaae_card = [];
 
-for(let i = 0; i<20 ; i++){
-    p1_cards.push(cards[i]);
-    cards.shift();
-    p2_cards.push(cards[i]);
-    cards.shift();
+const randomCard = function(){
+    for(let i = 0; i<20 ; i++){
+        p1_cards.push(cards[i]);
+        cards.shift(); 
+        p2_cards.push(cards[i]);
+        // console.log(cards);
+        cards.shift();
+        // console.log(cards);
+    }
 }
-console.log(cards);
-console.log(p1_cards);
-console.log(p2_cards);
+randomCard(); 
+
+const cardToPlayer = function(){
+    for(let i = 0; i<4 ; i++){
+        document.querySelector(`.p1-card${i+1}>p`).innerHTML = p1_cards[i];
+        document.querySelector(`.p2-card${i+1}>p`).innerHTML = p2_cards[i];
+    }
+    
+}
+cardToPlayer();
+
+// card to action area
+
+const cardToAction = function(){
+    
+    const elms = document.querySelectorAll('.card')
+    
+    elms.forEach( (elm)=>{
+        elm.addEventListener('click', ()=>{
+            const elmContent = Number (elm.textContent);
+            const whichPlayer = Number (elm.classList[1][1]);
+            console.log(`${elmContent}` , elm , whichPlayer, typeof(whichPlayer))
+            
+
+            
+            document.querySelector('.action-cards').insertAdjacentHTML(`beforeend`,`<div class="card action-card-i"><p>${elmContent}</p></div>`)
+            qaae_card.push(elmContent)
+            console.log(document.querySelector('.action-cards').innerHTML);
+            elm.remove();
+        })
+    }
+    )
+    
+    
+}
+
+cardToAction();
 
 
 
--- todo.md
+++ todo.md
@@ -1,10 +1,34 @@
-[] A simple ui for the app using vanilla css (90's style)
-[] Use css Grid and Flexbox
+[x] A simple ui for the app using vanilla css (90's style)
+[x] Use css Grid and Flexbox
 ============================== Game Logic ==============================  
- 
+
+[] a quick ui to let the user choose his name
+[] Player can choose his name and the who against him's name !
 [] Play with a robot
 [] Wim Lose state
 [] inGame options
-[] Player can choose his name and the who against him's name !
 [] points system
 [] animations
+
+
+============= random card logic =======
+
+[1] 40 cards
+[2] 8 every round
+[3] there is 4 place holders for cards: 
+    (1) : tfriqa
+    (2) : player-cards
+    (3) : qaae
+    (4) : player-saved-cards
+[4] every card must puts on these four steps  
+
+
+============ giving cards to players ================
+
+[x] make a loop that grab every item on the array and change its place randomly
+
+=============== game logic =========
+
+[] the player make smth to collect his luck on who's gonna play first
+[] when the first player drop a card he must wait for his enemy to drop his own as well
+[] if the player has sme card two/three times got points
